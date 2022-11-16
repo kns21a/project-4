5fc6907c6ce8f83684a7d8fabebb54c35c9d8764
Kayla Simpson <kns21a@fsu.edu>
2022-11-16 12:04:44 -0500

    created sketch.js and added comments

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1319, 695);
 }
 
 function draw() {
   background(125);
+
+  //add mouse clicked or button funtion to transition from sketch to sketch
+// add sound file to index.html for audio input/output
+//add sketch manager to index.html for muti-layerd sketxhes
 }
